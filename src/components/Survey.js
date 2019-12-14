c375e1e9b92e469b4fee617274a7520dcdc549dd
unknown <gbubemimakpokpomi@gmail.com>
2019-12-14 10:55:31 -0100

    Initial commit

---
+++ src/components/Survey.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Survey extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Welcome to the survey page</h1>
+            </div>
+        )
+    }
+}
+
+export default Survey;
