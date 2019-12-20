1de70243991099aebead2b065e3127eeb12da4fc
Syssoft Consulting <admin@example.com>
2019-12-20 15:36:50 +0100

    "feature(signup-page):create signup page To create Input component To create Button component To create Copyright component To create label component To create the signup page"

---
+++ src/components/copyright/Copyright.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Copyright.css";
+
+const Copyright = () => {
+  return (
+    <small id="copy" className="form-text text-center copyright">
+      Copyright SurveyPoint 2019
+    </small>
+  );
+};
+
+export default Copyright;
---
+++ src/components/label/Label.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Label = props => {
+  const { forLabel } = props;
+  return (
+    <label className="form-label" for={forLabel}>
+      {props.children}
+    </label>
+  );
+};
+
+export default Label;
