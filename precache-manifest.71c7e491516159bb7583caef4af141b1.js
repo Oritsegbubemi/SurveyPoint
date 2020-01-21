057710bd859459efb3102c6b6bed566a0e67ce33
Damilola Adekoya <damilolaadekoya@Damilolas-MacBook-Pro.local>
2020-01-22 00:25:21 +0100

    Updates

---
+++ precache-manifest.71c7e491516159bb7583caef4af141b1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52dee3e89296fe6bf30c14afa7f55ed6",
+    "url": "/SurveyPoint/index.html"
+  },
+  {
+    "revision": "802503656948436af332",
+    "url": "/SurveyPoint/static/css/main.3d87707a.chunk.css"
+  },
+  {
+    "revision": "7245f776c029c343e5ea",
+    "url": "/SurveyPoint/static/js/2.d8bcce3a.chunk.js"
+  },
+  {
+    "revision": "5586a95853ee43deda1fb4da37dc3fc8",
+    "url": "/SurveyPoint/static/js/2.d8bcce3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "802503656948436af332",
+    "url": "/SurveyPoint/static/js/main.e05ff0f7.chunk.js"
+  },
+  {
+    "revision": "0e1ed3618cc9afcdeaf6",
+    "url": "/SurveyPoint/static/js/runtime-main.cfc69408.js"
+  },
+  {
+    "revision": "4c79618af530e0b766a9022cbde4d28e",
+    "url": "/SurveyPoint/static/media/Logo.4c79618a.png"
+  },
+  {
+    "revision": "58e554eee6d19fb6ba397695be525eab",
+    "url": "/SurveyPoint/static/media/banner.58e554ee.jpeg"
+  },
+  {
+    "revision": "b1ec10fc4a12bdf4f314c2f9718f1113",
+    "url": "/SurveyPoint/static/media/banner2.b1ec10fc.jpeg"
+  },
+  {
+    "revision": "f08ecca5341fd8fe7654c9e5c815304f",
+    "url": "/SurveyPoint/static/media/surveypointlogo.f08ecca5.jpeg"
+  }
+]);
